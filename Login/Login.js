13f7c8aa4088e03466c71e7e604ced5415f708bc
Parasuramandckap <parasuaptadckap@gmail.com>
2023-06-20 21:34:10 +0530

    login page validation completed

--- Login/Login.js
+++ Login/Login.js
@@ -17,3 +17,36 @@ passwordicon.addEventListener("click", (e) => {
     }
 
 })
+
+
+let EmailId =  document.querySelector("#emailId")
+let Password = document.querySelector("#password")
+
+let form = document.querySelector("form")
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+    let emailVal = EmailId.value.trim();
+    let PasswordVal = Password.value.trim()
+
+    if(emailVal === ""){
+        seterror(EmailId)
+    }else{
+        setsucces(EmailId)
+    }
+
+    if(PasswordVal === ""){
+        seterror(Password)
+    }
+    else{
+        setsucces(Password)
+    }
+})
+
+function seterror(element){
+    element.style.border = "1px solid hsl(14, 100%, 33%, 100%)"
+}
+function setsucces(element){
+    element.style.border = "1px solid white"
+}
