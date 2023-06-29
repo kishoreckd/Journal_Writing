6dd4e007d54128c494f71bc3f198c464302eaf97
Parasuramandckap <parasuaptadckap@gmail.com>
2023-06-29 16:27:36 +0530

    completed toast notification in login page

--- Login/Login.js
+++ Login/Login.js
@@ -23,7 +23,7 @@ let EmailId =  document.querySelector("#emailId")
 let Password = document.querySelector("#password")
 
 let form = document.querySelector("form")
-
+let alertMessage = document.querySelector(".text-2")
 form.addEventListener("submit",(e)=>{
     e.preventDefault()
 
@@ -31,22 +31,51 @@ form.addEventListener("submit",(e)=>{
     let PasswordVal = Password.value.trim()
 
     if(emailVal === ""){
-        seterror(EmailId)
-    }else{
-        setsucces(EmailId)
+        seterror(alertMessage,"Email feild required")
     }
-
-    if(PasswordVal === ""){
-        seterror(Password)
+    else if(PasswordVal === ""){
+        seterror(alertMessage,"password feild required")
     }
     else{
-        setsucces(Password)
+        console.log("all okay");
+    }
+    if(emailVal === "" && PasswordVal === ""){
+        seterror(alertMessage,"All feild required")
     }
 })
 
-function seterror(element){
-    element.style.border = "1px solid hsl(14, 100%, 33%, 100%)"
+function seterror(element,message){
+    element.innerText = message;
+    toastNotification();
 }
-function setsucces(element){
-    element.style.border = "1px solid white"
+
+//toast notfication completed      
+toast = document.querySelector(".toast")
+closeIcon = document.querySelector(".close"),
+progress = document.querySelector(".progress");
+
+let timer1, timer2;
+
+function toastNotification(){
+    toast.classList.add("active");
+    progress.classList.add("active");
+
+    timer1 = setTimeout(() => {
+        toast.classList.remove("active");
+    }, 5000);
+
+    timer2 = setTimeout(() => {
+      progress.classList.remove("active");
+    }, 5300);
 }
+
+closeIcon.addEventListener("click", () => {
+    toast.classList.remove("active");
+    
+    setTimeout(() => {
+      progress.classList.remove("active");
+    }, 300);
+
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+  });
